9bdebfa3fb0036f70ae85437dc184763319d80e5
XimenaGG <ximenagg2007@gmail.com>
2026-05-24 23:25:16 -0500

    ImLPasar de reporte zona a punto mapa #9

--- src/main/java/utec/reciscore/reporteZona/application/ReporteZonaController.java
+++ src/main/java/utec/reciscore/reporteZona/application/ReporteZonaController.java
@@ -2,13 +2,12 @@ package utec.reciscore.reporteZona.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import utec.reciscore.reporteZona.dto.ReporteZonaRequestDTO;
 import utec.reciscore.reporteZona.dto.ReporteZonaResponseDTO;
 import utec.reciscore.reporteZona.model.ReporteZonaService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reportes-zona")
@@ -22,6 +21,14 @@ public class ReporteZonaController {
     ) {
         return ResponseEntity.ok(
                 reporteZonaService.create(dto)
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReporteZonaResponseDTO>> getAll() {
+
+        return ResponseEntity.ok(
+                reporteZonaService.getAll()
         );
     }
 
