14615841ebf5d483f47dfb4ee3f84342478edb62
Luis Enrique <tunombre@correo.com>
2026-05-30 10:56:50 -0500

    mapper en el ranking

--- src/main/java/utec/reciscore/ranking/dto/RankingResponseDTO.java
+++ src/main/java/utec/reciscore/ranking/dto/RankingResponseDTO.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RankingResponseDTO {
     private Long userId;
     private String username;
--- src/main/java/utec/reciscore/ranking/model/RankingService.java
+++ src/main/java/utec/reciscore/ranking/model/RankingService.java
@@ -1,6 +1,7 @@
 package utec.reciscore.ranking.model;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import utec.reciscore.ranking.dto.RankingResponseDTO;
 import utec.reciscore.ranking.infrastructure.RankingRepository;
@@ -14,21 +15,18 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class RankingService {
 
     private final RankingRepository rankingRepository;
+    private final ModelMapper modelMapper;
 
     public List<RankingResponseDTO> getRanking() {
         List<User> usuarios = rankingRepository.findAllOrderByPointsDesc();
         AtomicInteger posicion = new AtomicInteger(1);
 
         return usuarios.stream()
-                .map(u -> new RankingResponseDTO(
-                        u.getId(),
-                        u.getUsername(),
-                        u.getName(),
-                        u.getPoints(),
-                        u.getLocation(),
-                        u.getNivel(),
-                        posicion.getAndIncrement()
-                ))
+                .map(u -> {
+                    RankingResponseDTO dto = modelMapper.map(u, RankingResponseDTO.class);
+                    dto.setPosicion(posicion.getAndIncrement());
+                    return dto;
+                })
                 .toList();
     }
 
@@ -37,15 +35,11 @@ public class RankingService {
         AtomicInteger posicion = new AtomicInteger(1);
 
         return usuarios.stream()
-                .map(u -> new RankingResponseDTO(
-                        u.getId(),
-                        u.getUsername(),
-                        u.getName(),
-                        u.getPoints(),
-                        u.getLocation(),
-                        u.getNivel(),
-                        posicion.getAndIncrement()
-                ))
+                .map(u -> {
+                    RankingResponseDTO dto = modelMapper.map(u, RankingResponseDTO.class);
+                    dto.setPosicion(posicion.getAndIncrement());
+                    return dto;
+                })
                 .toList();
     }
 }
